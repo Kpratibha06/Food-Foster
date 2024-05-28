621098a5b2c837b61156048f5ea103240c7c0f36
Kumari Pratibha <81520509+Kpratibha06@users.noreply.github.com>
2024-05-28 13:26:52 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -22,7 +22,7 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const facebookdata = await fetch(
-      "https://www.facebook.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.walmart.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     const facebookjson = await facebookdata.json();
